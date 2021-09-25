cc89fff5bb422707c8f52adc5d2e8333c04af54d
Kota <kouta.s0817@icloud.com>
2021-09-26 01:33:30 +0900

    ユーザー登録機能の実装

---
+++ backend/api/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
