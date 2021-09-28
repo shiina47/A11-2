ffaac507dd15eb1b6714d4977b776da7db6e6500
Kota <kouta.s0817@icloud.com>
2021-09-29 03:24:17 +0900

    /auth への画面遷移

--- frontend/src/components/pages/Header.jsx
+++ frontend/src/components/pages/Header.jsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+import { useHistory } from "react-router-dom";
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,26 +9,31 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export const Header = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" color="secondary">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="center">
-            Reciper
-          </Typography>
-          <Button color="inherit">ログイン</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+export const Header = memo(() => {
+    const history = useHistory();
+    const toLogIn = () => {
+        history.push("/auth");
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="secondary">
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="center">
+                Reciper
+            </Typography>
+            <Button color="inherit" onClick={toLogIn}>ログイン</Button>
+            </Toolbar>
+        </AppBar>
+        </Box>
+    );
+});
