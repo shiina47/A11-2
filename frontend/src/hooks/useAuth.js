66c63545f4e8a9aa8e8197e826cb50b73239fb47
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-01 21:40:37 +0900

    ログイン・登録のカスタムフック作成

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,46 @@
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+export const useAuth = () => {
+  const history = useHistory();
+
+  const login = useCallback(
+    (data) => {
+      axios
+        .post("http://127.0.0.1:8000/authen/jwt/create", data, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          localStorage.setItem("localJWT", res.data.access);
+          history.push("/top");
+        })
+        .catch(() => {
+          alert("ログインできません");
+        });
+    },
+    [history]
+  );
+
+  const register = useCallback(
+    (data) => {
+      axios
+        .post("http://127.0.0.1:8000/api/register/", data, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(() => {
+          login(data);
+        })
+        .catch(() => {
+          alert("作成できません");
+        });
+    },
+    [login]
+  );
+
+  return { login, register };
+};
