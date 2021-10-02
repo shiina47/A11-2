a1f58e195c73b252248f3da86287159921e44df7
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-02 16:32:34 +0900

    簡単な画面作成

--- frontend/src/components/pages/Post.jsx
+++ frontend/src/components/pages/Post.jsx
@@ -1,5 +1,26 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import { TextField } from "@mui/material";
 
 export const Post = memo(() => {
-  return <p>レシピ投稿ページ</p>;
+  const [recipe, setRecipe] = useState({
+    title: "",
+    cost: 0,
+    minutes: 0,
+    image: null,
+  });
+  return (
+    <>
+      <div>
+        <p>レシピ投稿</p>
+        <TextField label="title" />
+        <TextField label="cost" />
+        <TextField label="minutes" />
+        <input type="file" />
+        <br />
+        <TextField label="material" />
+        <br />
+        <TextField label="process" />
+      </div>
+    </>
+  );
 });
