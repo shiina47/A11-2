a0d1acd0cecf13e64c085ff05b87eb1649a4f389
Sheena <codegym47@gmail.com>
2021-10-06 05:53:26 +0900

    ボタン丸くなった

--- frontend/src/components/loginConponents/login.jsx
+++ frontend/src/components/loginConponents/login.jsx
@@ -5,15 +5,15 @@ import Button from "@mui/material/Button";
 
 export default function Login() {
   return (
-    <Box sx={{display :"flex" , alignItems: "center" , flexDirection: "column",}}>
+    <Box sx={{display :"flex" , alignItems: "center" , flexDirection: "column" }}>
         <h1 style={{ fontSize: "50px" }}>Reciper</h1>
         <h3>ログイン</h3>
-        <Box sx={{ flexGrow: 0 }}>
+        <Box>
           <TextField sx={{margin: "8px"}} fullWidth label="メールアドレス" id="fullWidth" />
           <TextField sx={{margin: "8px"}} fullWidth label="パスワード" id="fullWidth" />
         </Box>
-        <Box>
-          <Button　sx={{margin: "8px 0px"}} variant="contained">
+        <Box sx={{width: "100%"}}>
+          <Button　sx={{margin: "8px 0px" , borderRadius: "50px"  , width: "100%"}} variant="contained">
             ログイン
           </Button>  
         </Box>  
--- frontend/src/components/loginConponents/register.jsx
+++ frontend/src/components/loginConponents/register.jsx
@@ -5,7 +5,7 @@ import Button from "@mui/material/Button";
 
 export default function Register() {
   return (
-    <Box sx={{display :"flex" , alignItems: "center" , flexDirection: "column",}}>
+    <Box sx={{display :"flex" , alignItems: "center" , flexDirection: "column" , }}>
       <h1 style={{ fontSize: "50px" }}>Reciper</h1>
       <h3>ユーザー登録</h3>
       <Box>
@@ -13,10 +13,10 @@ export default function Register() {
         <TextField sx={{margin: "8px"}} fullWidth label="パスワード" id="fullWidth" />
         <TextField sx={{margin: "8px"}} fullWidth label="パスワード（確認）" id="fullWidth" />
       </Box>
-      <Box>
-        <Button sx={{margin: "8px 0px"}} variant="contained">
+      <Box sx={{width: "100%"}}>
+        <Button sx={{margin: "8px 0px" , borderRadius: "50px" , width: "100%"}} variant="contained">
           ユーザー登録
-        </Button>  
+        </Button>
       </Box>  
     </Box>
   );
