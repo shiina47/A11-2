b5c6d6cd9c8c81aab36503613d5ff0756c2f7abe
Kota <kouta.s0817@icloud.com>
2021-10-06 09:07:17 +0900

    レシピ一覧ページで簡単に情報を表示

--- frontend/src/components/pages/RecipeDisplay.jsx
+++ frontend/src/components/pages/RecipeDisplay.jsx
@@ -1,5 +1,32 @@
-import { memo } from "react";
+import { memo, useState, useEffect } from "react";
+import axios from "axios";
+import Recipe from "../Recipe";
 
 export const RecipeDisplay = memo(() => {
-  return <p>レシピを表示しているページ</p>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/recipe")
+      .then((res) => {
+        setRecipes(res.data);
+      })
+  }, []);
+
+  return (
+    <>
+      {recipes.map((recipe) => (
+        <Recipe
+          title={recipe.title}
+          cost={recipe.cost}
+          amount={recipe.amount}
+          minutes={recipe.minutes}
+          image={recipe.image}
+          material={recipe.material}
+          process={recipe.process}
+          key={recipe.title}
+        />
+      ))}
+    </>
+  );
 });
