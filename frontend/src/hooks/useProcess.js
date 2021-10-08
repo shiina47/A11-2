85175b1f4503d47a763f6a7dff87eb9724b60d18
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-08 21:47:20 +0900

    登録関連のバグ修正

--- frontend/src/hooks/useProcess.js
+++ frontend/src/hooks/useProcess.js
@@ -4,25 +4,18 @@ import axios from "axios";
 
 export const useProcess = () => {
   const createProcess = useCallback(async (data, id) => {
-    console.log(id);
-    const res = await axios
-      .post(
-        "http://127.0.0.1:8000/api/process/",
-        { order: data.order, how_to: data.how_to, recipe: id },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `JWT ${localStorage.localJWT}`,
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch(() => {
-        alert("失敗");
-      });
-    return res;
+    const res = await axios.post(
+      "http://127.0.0.1:8000/api/process/",
+      { order: data.order, how_to: data.how_to, recipe: id },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      }
+    );
+
+    return res.data;
   }, []);
 
   return {
