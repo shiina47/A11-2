f0dbdb61e9a4212fae891cbfcbbae350b196fc14
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-12 05:12:11 +0900

    login user情報取得(バックエンド)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
-from api.views import CreateUserView, RecipeViewSet, ProcessViewSet, MaterialViewSet, MyPageViewSet
+from api.views import CreateUserView, RecipeViewSet, ProcessViewSet, MaterialViewSet, MyPageViewSet, MyProfileView
 
 app_name = 'user'
 
@@ -13,6 +13,7 @@ router.register('mypage', MyPageViewSet, basename='mypage')
 
 
 urlpatterns = [
+    path('myself/', MyProfileView.as_view(), name="myself"),
     path('register/', views.CreateUserView.as_view(), name='register'),
     path('', include(router.urls)),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -10,12 +10,20 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
 
 
+class MyProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
 
 class MyPageViewSet(viewsets.ModelViewSet):
     queryset = MyPage.objects.all()
@@ -24,9 +32,11 @@ class MyPageViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(userPage=self.request.user)
 
+
 class ProcessViewSet(viewsets.ModelViewSet):
     queryset = Process.objects.all()
     serializer_class = ProcessSerializer
+
 
 class MaterialViewSet(viewsets.ModelViewSet):
     queryset = Material.objects.all()
