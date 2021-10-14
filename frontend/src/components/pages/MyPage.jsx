548b3cc8d1c6d4614a7bf754f6f9576989466144
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-14 09:14:24 +0900

    投稿したレシピの表示

--- frontend/src/components/pages/MyPage.jsx
+++ frontend/src/components/pages/MyPage.jsx
@@ -1,5 +1,37 @@
-import { memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+
+import { useRecipe } from "../../hooks/useRecipe";
 
 export const MyPage = memo(() => {
-  return <p>マイページ</p>;
+  const { getMyRecipes, myRecipes } = useRecipe();
+
+  useEffect(() => {
+    getMyRecipes();
+  }, []);
+
+  return (
+    <Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+          投稿したレシピ
+        </Typography>
+        <div>
+          {myRecipes.map((myRecipe) => {
+            return (
+              <div key={myRecipe.id}>
+                <p>{myRecipe.title}</p>
+              </div>
+            );
+          })}
+        </div>
+      </Box>
+    </Box>
+  );
 });
