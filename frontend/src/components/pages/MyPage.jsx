798fe052b66ea001c52dcfb7219916e1a2813d12
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-15 07:59:11 +0900

    投稿したレシピ、お気に入りのレシピ表示

--- frontend/src/components/pages/MyPage.jsx
+++ frontend/src/components/pages/MyPage.jsx
@@ -5,28 +5,78 @@ import { Box, Typography, List, ListItem, ListItemText } from "@mui/material";
 import { useRecipe } from "../../hooks/useRecipe";
 
 export const MyPage = memo(() => {
-  const { getMyRecipes, myRecipes } = useRecipe();
+  const { getMyRecipes, myRecipes, getMyLikedRecipes, myLikedRecipes } =
+    useRecipe();
 
   useEffect(() => {
     getMyRecipes();
+    getMyLikedRecipes();
   }, []);
 
   return (
-    <Box>
+    <Box width="90%" mx="auto">
       <Box
         display="flex"
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
       >
-        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-          投稿したレシピ
-        </Typography>
+        <Box
+          sx={{ borderBottom: 2 }}
+          display="flex"
+          justifyContent="center"
+          width="350px"
+        >
+          <Typography
+            sx={{ mt: 4, mb: 2 }}
+            variant="h6"
+            component="div"
+            borderBottom="1"
+            fontWeight="bold"
+          >
+            投稿したレシピ
+          </Typography>
+        </Box>
+
         <List>
           {myRecipes.map((myRecipe) => {
             return (
               <ListItem key={myRecipe.id} button>
                 <ListItemText primary={myRecipe.title} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          sx={{ borderBottom: 2 }}
+          display="flex"
+          justifyContent="center"
+          width="350px"
+        >
+          <Typography
+            sx={{ mt: 4, mb: 2 }}
+            variant="h6"
+            component="div"
+            borderBottom="1"
+            fontWeight="bold"
+          >
+            お気に入りのレシピ
+          </Typography>
+        </Box>
+
+        <List>
+          {myLikedRecipes.map((myLikedRecipe) => {
+            return (
+              <ListItem key={myLikedRecipe.id} button>
+                <ListItemText primary={myLikedRecipe.title} />
               </ListItem>
             );
           })}
