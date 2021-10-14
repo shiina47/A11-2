4961ce8d8dddc134afa60dfb095479bd9079b296
Sheena <codegym47@gmail.com>
2021-10-15 02:35:04 +0900

    Topページ作成

--- frontend/src/components/pages/Top.jsx
+++ frontend/src/components/pages/Top.jsx
@@ -1,8 +1,9 @@
 import { memo } from "react";
 import { useHistory } from "react-router-dom";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import { useAuth } from "../../hooks/useAuth";
+import img from "../photo-1482049016688-2d3e1b311543.jpeg";
 
 export const Top = memo(() => {
   const history = useHistory();
@@ -17,24 +18,54 @@ export const Top = memo(() => {
 
   return (
     <>
-      <Grid container alignItems="center" direction="column">
-        <Grid item xs={8}>
-          <p>早いレシピを早く届ける</p>
-        </Grid>
-        <Grid item xs={8}>
-          <Button variant="contained" onClick={toPost}>
-            レシピを投稿する
-          </Button>
-        </Grid>
-        <Grid item xs={8}>
-          <Button variant="contained" onClick={toRecipes}>
-            レシピを見つける
-          </Button>
-        </Grid>
-      </Grid>
-      <Button variant="contained" onClick={getUserId}>
-        ゆーざーIDゲット ボタン
-      </Button>
+      <Box sx={{ display: "flex", justifyContent: "center" , backgroundImage: `url(${img})` , backgroundSize:"cover" , height:"calc(100vh - 64px)"}}>
+        <Box
+          sx={{
+            width: "300px",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              margin: "40px 0 136px 0"
+            }}
+          >
+            <h1 style={{ color: "white", fontSize: "48px" }}>Reciper</h1>
+            <p style={{ color: "white", letterSpacing: "4px" }}>
+              早いレシピを
+              <br />
+              早く見つける
+            </p>
+          </Box>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              flexDirection: "column"
+            }}
+          >
+            <Button
+              variant="contained"
+              sx={{ marginBottom: "8px", width: "60%" }}
+              onClick={toPost}
+            >
+              レシピを投稿する
+            </Button>
+            <Button variant="contained" sx={{ marginBottom: "8px" , width: "60%" }} onClick={toRecipes} >
+              レシピを見つける
+            </Button>
+            <Button variant="contained" sx={{width: "60%"}} onClick={getUserId}>
+              ゆーざーIDゲット ボタン
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 });
