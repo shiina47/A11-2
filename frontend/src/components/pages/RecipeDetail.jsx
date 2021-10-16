5e46a0bccbf5e8e32765f76165dc46b46334e97f
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-17 08:37:28 +0900

    選択したレシピ取得関数実装

--- frontend/src/components/pages/RecipeDetail.jsx
+++ frontend/src/components/pages/RecipeDetail.jsx
@@ -1,12 +1,19 @@
-import { memo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { useRecipe } from "../../hooks/useRecipe";
 
 export const RecipeDetail = memo(() => {
   const location = useLocation();
-  console.log(location.state);
-  return (
-    <>
-      <p> detail</p>
-    </>
-  );
+  const recipeId = location.state.id;
+
+  const { getSelectedRecipe, selectedRecipe } = useRecipe();
+
+  useEffect(() => {
+    getSelectedRecipe(recipeId);
+  }, []);
+
+  return <Box></Box>;
 });
