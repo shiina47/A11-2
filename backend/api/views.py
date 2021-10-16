3c1e2dbc09bfed67deaa04096c4ce5965b7ef443
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-16 11:30:36 +0900

    isDisplayed field 追加

--- backend/api/views.py
+++ backend/api/views.py
@@ -20,7 +20,7 @@ class MyProfileView(generics.RetrieveUpdateAPIView):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    filter_fields = ('user', 'liked')
+    filter_fields = ('user', 'liked', 'idDisplayed')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
