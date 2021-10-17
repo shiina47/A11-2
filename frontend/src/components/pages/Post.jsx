1c0c94ea5d05a5933eba25e45ec18b3334823ec7
Tomoya18xxx <tomoya18xxx@gmail.com>
2021-10-17 13:41:33 +0900

    画面遷移図のようにリファクタリング

--- frontend/src/components/pages/Post.jsx
+++ frontend/src/components/pages/Post.jsx
@@ -1,13 +1,15 @@
 import { memo, useState } from "react";
-import { TextFieldMaterial } from "../atoms/TextFieldMaterial";
-import { TextFieldProcess } from "../atoms/TextFieldProcess";
+import { Box, Stack, IconButton, Typography } from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+
+import { TextFieldMaterial } from "../Post/TextFieldMaterial";
+import { TextFieldProcess } from "../Post/TextFieldProcess";
 import { useRecipe } from "../../hooks/useRecipe";
 import { useMaterial } from "../../hooks/useMaterial";
 import { useProcess } from "../../hooks/useProcess";
-
-import { TextField, Box, Button, Stack, IconButton, Grid } from "@mui/material";
-import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import { useHistory } from "react-router-dom";
+import { InputField } from "../atoms/InputField";
+import { PrimaryBtn } from "../atoms/PrimaryBtn";
 
 const initialState = {
   title: "",
@@ -62,7 +64,9 @@ export const Post = memo(() => {
   return (
     <>
       <Box my={4} mx="auto" width={300}>
-        <h2>レシピ投稿</h2>
+        <Typography fontWeight="bold" variant="h5" color="text.primary">
+          レシピ投稿
+        </Typography>
         <Box>
           <Stack spacing={2}>
             <input
@@ -76,41 +80,77 @@ export const Post = memo(() => {
             <IconButton onClick={handlerEditPicture} size="large">
               <AddPhotoAlternateIcon color="secondary" fontSize="large" />
             </IconButton>
-            <Grid container spacing={2}>
-              <h4>タイトル</h4>
-              <Grid item xs={12}>
-                <TextField
-                  name="title"
-                  placeholder="例：親子丼"
-                  value={recipeForm.title}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={10}>
-                <TextField
-                  type="number"
-                  name="cost"
-                  placeholder="例：100"
-                  value={recipeForm.cost}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <p>円</p>
-              </Grid>
-              <Grid item xs={10}>
-                <TextField
+
+            <Box>
+              <Typography
+                fontWeight="bold"
+                variant="body1"
+                color="text.primary"
+              >
+                タイトル
+              </Typography>
+              <InputField
+                type="text"
+                name="title"
+                placeholder="例：親子丼"
+                value={recipeForm.title}
+                onChange={handleChange}
+              />
+            </Box>
+            <Box>
+              <Typography
+                fontWeight="bold"
+                variant="body1"
+                color="text.primary"
+              >
+                調理時間
+              </Typography>
+              <Box display="flex">
+                <InputField
                   type="number"
                   name="minutes"
                   placeholder="例：5"
                   value={recipeForm.minutes}
                   onChange={handleChange}
                 />
-              </Grid>
-              <Grid item xs={2}>
-                <p>分</p>
-              </Grid>
-            </Grid>
+
+                <Typography
+                  variant="body1"
+                  color="text.secondary"
+                  fontWeight="bold"
+                  alignSelf="flex-end"
+                >
+                  分
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box>
+              <Typography
+                fontWeight="bold"
+                variant="body1"
+                color="text.primary"
+              >
+                費用
+              </Typography>
+              <Box display="flex">
+                <InputField
+                  type="number"
+                  name="cost"
+                  placeholder="例：100"
+                  value={recipeForm.cost}
+                  onChange={handleChange}
+                />
+                <Typography
+                  variant="body1"
+                  color="text.secondary"
+                  fontWeight="bold"
+                  alignSelf="flex-end"
+                >
+                  円
+                </Typography>
+              </Box>
+            </Box>
           </Stack>
         </Box>
 
@@ -123,10 +163,8 @@ export const Post = memo(() => {
 
         <TextFieldProcess processes={processes} setProcesses={setProcesses} />
 
-        <Box mt={4}>
-          <Button sx={{ width: "100%" }} onClick={onSubmit} variant="contained">
-            投稿する
-          </Button>
+        <Box display="flex" justifyContent="center" mt={4}>
+          <PrimaryBtn onClick={onSubmit}>投稿する</PrimaryBtn>
         </Box>
       </Box>
     </>
